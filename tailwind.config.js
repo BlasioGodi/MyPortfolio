c2fd5ac69ae3adfd59689726e1948341789abaee
muhindablasio@gmail.com <muhindablasio@gmail.com>
2022-12-28 10:17:15 +0300

    Reinitialized files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: ['./src/**/*.html',
     './src/**/*.js',
     './public/**/*.html',
-    './public/**/*.js'],
+    './public/**/*.js',
+    './index.html'],
   theme: {
     extend: {},
   },
